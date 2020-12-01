2504ff853f7df81cc5a5e7bb91877e6a3d696815
kirby0113 <2000ryo0113@ezweb.ne.jp>
2020-12-01 18:29:27 +0900

    Workのmigrationファイルの設定

--- database/migrations/2020_11_30_233612_create_works_table.php
+++ database/migrations/2020_11_30_233612_create_works_table.php
@@ -14,7 +14,12 @@ class CreateWorksTable extends Migration
     public function up()
     {
         Schema::create('works', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('Work_id');
+            $table->integer('Client_id');
+            $table->integer('Contractor_id');
+            $table->integer('Category_id');
+            $table->string('Title');
+            $table->longText('Contents');
             $table->timestamps();
         });
     }
