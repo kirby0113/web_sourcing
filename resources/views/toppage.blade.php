917de010ea6ee9331f9d18c07df4bdb596b11637
kirby0113 <2000ryo0113@ezweb.ne.jp>
2020-12-04 11:32:20 +0900

    検索バーの表示

--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -8,6 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <!-- <link rel = "stylesheet" type="text/css" href="{{asset('css/toppage.css')}}"> -->
@@ -17,10 +19,9 @@
             padding: 0px;
         }
             div#header {
-                
+                float:left;
                 background-color: #8888FF;
-                text-align: left;
-                display: inline-block;
+                
                 margin: 3px;
             }
             div.title{
@@ -29,6 +30,17 @@
                 font-size: 350%;
                 padding: 20px;
             }
+            div#search-bar{
+                float:left;
+                
+            }
+            input.bar{
+                padding: 2px;
+                font-size: 200%;
+            }
+            input.search{
+                font-size:180%;
+            }
         </style>
     </head>
     <body>
@@ -36,6 +48,11 @@
             
             <div id="header"><div class="title">WebSourcing</div></div>
 
+            <div id="search-bar">
+                <input type="text" name="search-word" class="bar">
+                <input type="submit" name="submit" value="検索" class="search">
+            </div>
+       
        </div>
     </body>
 </html>
