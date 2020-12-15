9b9f95ca19e6eb74903a99ae849ec6c08a0cb1db
kirby0113 <2000ryo0113@ezweb.ne.jp>
2020-12-15 15:16:43 +0900

    work_detailへのルーティングの作成

--- app/Http/Controllers/work_detailController.php
+++ app/Http/Controllers/work_detailController.php
@@ -7,4 +7,7 @@ use Illuminate\Http\Request;
 class work_detailController extends Controller
 {
     //
+    public function index(){
+        return view('work_detail');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/mypage','mypageController@index');
+Route::get('/work_detail','work_detailController@index');
