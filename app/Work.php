5c395512b5b98a1d9972d0197933d7f000cdc7ec
kirby0113 <2000ryo0113@ezweb.ne.jp>
2020-12-31 23:51:08 +0900

    Workに検索用メソッドの追加

--- app/Work.php
+++ app/Work.php
@@ -8,4 +8,10 @@ class Work extends Model
 {
     //
     protected $primaryKey = 'Work_id';
+    protected $table = "works";
+
+    public static function category_pickup($id){
+        return Work::where('Category_id',$id)->get();
+    }       
+    
 }
