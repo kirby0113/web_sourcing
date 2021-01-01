6e3c983fdf3b4ac783b56823cb668b8794f025cb
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-02 02:42:45 +0900

    Workデータ検索用メソッドの追加

--- app/Work.php
+++ app/Work.php
@@ -11,7 +11,16 @@ class Work extends Model
     protected $table = "works";
 
     public static function category_pickup($id){
-        return Work::where('Category_id',$id)->get();
+        return Work::where('Category_id',$id)->paginate(10);
     }       
+    
+    public static function word_pickup($words){
+        $query = Work::query();
+        foreach($words as $word){
+            $query->orwhere('Title','like','%'.$word.'%');
+            $query->orwhere('Contents','like','%'.$word.'%');
+        }
+        return $query->paginate(10);
+    }
     
 }
