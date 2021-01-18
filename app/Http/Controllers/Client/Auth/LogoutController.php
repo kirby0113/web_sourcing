1ba0fa3457607cf0e9e6ae740a650191543424dc
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-18 23:02:29 +0900

    Clientのログアウト機能を実装

---
+++ app/Http/Controllers/Client/Auth/LogoutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Client\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    //
+    public function logout(Request $request){
+        Auth::guard('client')->logout();
+        return redirect()->route('client.login');
+    }
+}
