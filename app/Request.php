f0b5645eea4ceda0eed524d71ed79cd979dfa00b
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-20 15:05:56 +0900

    リクエストモデルの作成

--- app/Request.php
+++ app/Request.php
@@ -3,10 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Contractor;
 class Request extends Model
 {
     //
     protected $primaryKey = 'Request_id';
     protected $table = "requests";
+
+
+    public function contractor(){
+        return $this->belongsTo(Contractor::class,'contractor_id');
+    }
+
+    public function getContentsAttribute($value){
+        $value = str_replace(array("\r\n", "\r", "\n"), "\n", $value);
+        return nl2br($value);
+    }
+
+    public static function ContentsExplode($value){
+        return explode("\n",$value);
+    }
 }
