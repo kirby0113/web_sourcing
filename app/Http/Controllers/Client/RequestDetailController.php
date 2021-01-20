76de1ba9d8bec6bf15953b6b8af727be94204a8f
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-20 15:05:40 +0900

    RequestDetailControllerの作成

--- app/Http/Controllers/Client/RequestDetailController.php
+++ app/Http/Controllers/Client/RequestDetailController.php
@@ -8,4 +8,12 @@ use Illuminate\Http\Request;
 class RequestDetailController extends Controller
 {
     //
+    public function index(Request $request){
+        
+        $work_requests = \App\Request::where('work_id',$request->id)->get();
+       /* foreach($work_requests as $request){
+            var_dump($request->contractor);
+        } */
+        return view('client.request_detail',compact('work_requests'));
+    }
 }
