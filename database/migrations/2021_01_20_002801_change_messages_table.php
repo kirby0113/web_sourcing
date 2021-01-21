92134abb2aeed97313b023f7a62460008911ad48
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-21 17:59:38 +0900

    Messageテーブルのカラム変更ようマイグレーションファイルの作成

--- database/migrations/2021_01_20_002801_change_messages_table.php
+++ database/migrations/2021_01_20_002801_change_messages_table.php
@@ -18,6 +18,8 @@ class ChangeMessagesTable extends Migration
             $table->bigInteger('room_id')->unsigned()->after('Message_id');
             $table->foreign('room_id')
             ->references('Room_id')->on('message_rooms');
+            $table->renameColumn('do_user_id','contractor_id');
+            $table->renameColumn('from_user_id','client_id');
 
         });
     }
@@ -31,6 +33,8 @@ class ChangeMessagesTable extends Migration
     {
         Schema::table('messages', function (Blueprint $table) {
             //
+            $table->renameColumn('contractor_id','do_user_id');
+            $table->renameColumn('client_id','from_user_id');
         });
     }
 }
