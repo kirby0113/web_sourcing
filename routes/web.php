bcfb5d4427c72ea1d75c600e1104b70c2aab4956
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-22 05:28:35 +0900

    Clientのメッセージルーム用ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,10 @@ Route::prefix('client')->namespace('Client')
     Route::get('/mypage','MypageController@index')->name('mypage');
     Route::get('/request_detail','RequestDetailController@index')->name('request_detail');
     Route::get('/contractor_show','MypageController@show')->name('contractor_show');
-            });
+    Route::get('/message_room_create','Message_RoomsController@store')->name('message_room_create');
+    Route::get('/message_room','Message_RoomsController@index')->name('message_room');
+    Route::post('/message_create','MessageController@store')->name('message_create');
+});
     Auth::routes();
     Route::get('/logout', 'Auth\LogoutController@logout')->name('client.logout');
 });
