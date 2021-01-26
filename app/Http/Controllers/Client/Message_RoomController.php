bcc2a43e3857cd5304dbe76e77687ff4be39ac99
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-26 18:04:23 +0900

    コントローラからの引数変更

--- app/Http/Controllers/Client/Message_RoomController.php
+++ app/Http/Controllers/Client/Message_RoomController.php
@@ -18,7 +18,7 @@ class Message_RoomController extends Controller
         $insert->finished = false;
         $insert->save();
         $room = DB::table('message_rooms')->orderBy('Room_id','desc')->first();
-         return redirect('/client/message_room?room_id='.$room_id);
+         return redirect('/client/message_room?room_id='.$room->Room_id);
     }
 
     public function index(Request $request){
@@ -26,7 +26,7 @@ class Message_RoomController extends Controller
         $room = Message_Room::find($request->room_id);
         $my_data = Auth::guard('client')->user();
         $my_id = $my_data->client_id;
-       // var_dump($messages);
+       //var_dump($messages);
        // var_dump($title);
         return view('client.message_room',compact('messages','my_id','room'));
     }
