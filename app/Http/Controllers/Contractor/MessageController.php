e6b1df516fc63f40f17ab8d0ad6b7b34aea91682
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-26 18:02:34 +0900

    Contractor用のメッセージコントローラを作成

---
+++ app/Http/Controllers/Contractor/MessageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Contractor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Message;
+use App\Message_Room;
+
+class MessageController extends Controller
+{
+    //
+    
+    public function store(Request $request){
+        $insert = new Message;
+        $room = Message_Room::find($request->room_id);
+       // var_dump($room);
+
+        $insert->room_id = $request->room_id;
+        $insert->contractor_id = $room->work->Contractor_id;
+        $insert->client_id = $room->work->Client_id;
+        $insert->Message = $request->message;
+        $insert->made = 'contractor';
+        $insert->save();
+        return redirect('/contractor/message_room?room_id='.$request->room_id);
+    }
+}
