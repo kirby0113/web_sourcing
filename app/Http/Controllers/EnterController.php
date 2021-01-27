680bf26c5d71697c86848197b9e5d29d14d97a0b
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-27 18:13:11 +0900

    enterコントローラを変更

--- app/Http/Controllers/EnterController.php
+++ app/Http/Controllers/EnterController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EnterController extends Controller
 {
     //
     public function index(){
-        return view('enter');
+                return view('enter');
+    }
+
+    public function contractor_index(){
+        if(Auth::guard('contractor')->check()){
+        return redirect('/contractor/toppage');
+        }else{
+            return view('enter');
+        }
+    }
+
+    public function client_index(){
+        if(Auth::guard('client')->check()){
+        return redirect('/client/toppage');
+        }else{
+            return view('enter');
+        }
     }
 }
