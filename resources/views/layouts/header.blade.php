2951ead1ddedf6aed8c25d6b94556f418e2795e9
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-27 18:11:45 +0900

    レイアウトファイル改善

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -38,7 +38,7 @@
     <nav class="navbar navbar-expand-md navbar-light shadow-sm">
         @yield('back')
             <div class="container">
-                <a class="navbar-brand" href="/enter">
+                <a class="navbar-brand" href="toppage">
                     {{ config('app.name', 'WebSourcing') }}
                 </a>
                 <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
