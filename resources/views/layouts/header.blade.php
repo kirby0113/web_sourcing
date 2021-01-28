fd68f86ddfa8e70a861df639e24c098ce50b43e0
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-01-28 16:39:57 +0900

    レイアウトファイル改善

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -33,6 +33,10 @@
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&display=swap" rel="stylesheet">
 <link rel = "stylesheet" type="text/css" href="{{asset('css/background.css')}}">
 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+      
+
 @yield('head')
     <header>
     <nav class="navbar navbar-expand-md navbar-light shadow-sm">
